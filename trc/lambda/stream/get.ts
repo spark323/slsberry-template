6b1f364ec60eb0bb87b8cadfac0002c0ce694d29
chris@reconlabs.ai <chris@reconlabs.ai>
2024-11-22 16:07:45 +0900

    cnyy

--- trc/lambda/stream/get.ts
+++ trc/lambda/stream/get.ts
@@ -11,74 +11,7 @@ import { AwsCredentialIdentityProvider } from "@smithy/types";
 import { Readable } from 'stream';
 import ddbUtil from '../libs/aws/ddbUtil.js';
 import { time } from "console";
-const querySchema = {
-    type: "object",
-    properties: {
-        pk: { type: "string", description: "Partition Key", },
-        sk: { type: "string", description: "Sort Key" },
-    },
-    required: ["pk"],
-    additionalProperties: false,
-} as const;
-// prettier-ignore
-const responseSchema = {
-    type: "object",
-    properties: {
-        message: { type: "string" },
-    },
-    additionalProperties: true,
-} as const;
-
-export const apiSpec = {
-    category: "Template",
-    event: [
-        {
-            type: "REST",
-            method: "GET",
-            path: "/template",
-        },
-        // {
-        //     type: "REST",
-        //     method: "POST",
-        //     path: "/v2/admin/templates",
-        //     authorizer: "recoAuthorizer",
-        // },
-    ],
-    timeout: 300,
-    url: {
-        cors: true
-    },
-    //---applicationsignal 모니터링을 위한 세팅
-    layers: [
-        //리전별로 다름 https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/CloudWatch-Application-Signals-Enable-Lambda.html#Enable-Lambda-Layers
-        "arn:aws:lambda:ap-northeast-2:615299751070:layer:AWSOpenTelemetryDistroJs:5"
-    ],
-    environment: {
-        AWS_LAMBDA_EXEC_WRAPPER: "/opt/otel-instrument"
-    },
-    //아래 내용을 해야 실제로 500에러를 badrequest 취급.
-    //--------------------------------------------------------------------------------
-    summary: "test template",
-    desc: "Template 을 생성합니다.",
-    // requestBody: {
-    //     required: true,
-    //     content: { "application/json": { schema: bodySchema } },
-    // },
-    requestQuery: querySchemaToParameters(querySchema),
-    errors: {},
-    responses: {
-        200: {
-            description: "",
-            content: { "application/json": { schema: responseSchema } },
-        },
-        400: { $ref: "#/components/responses/Validation" },
-        401: { $ref: "#/components/responses/Unauthorized" },
-        403: { $ref: "#/components/responses/InsufficientScope" },
-        404: { $ref: "#/components/responses/NotFound" },
-        415: { $ref: "#/components/responses/UnsupportedMediaType" },
-        500: { $ref: "#/components/responses/InternalServerError" },
-    },
-};
+import { querySchema, apiSpec, responseSchema } from "./get_header";
 
 const eventSchema = {
     type: "object",
