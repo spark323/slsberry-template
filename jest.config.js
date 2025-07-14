6e7bac3f524a2a3c192ab60d897c69ec59c82e5e
chris@reconlabs.ai <chris@reconlabs.ai>
2025-07-14 15:07:37 +0900

    config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+export default {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', {
+            useESM: true,
+        }]
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+};
